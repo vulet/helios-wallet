20de4d4637c76f66373062aad612e2ea95172ba3
vulet <spghtz@gmail.com>
2021-04-22 16:50:33 +0800

    fix(tailwindcss): remove pre-built

--- tasks/assets.js
+++ tasks/assets.js
@@ -1,11 +1,16 @@
 const { src, dest } = require('gulp');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const tailwindcss = require('tailwindcss');
 
 function copyHtml() {
   return src('app/renderer/index.html').pipe(dest('build/renderer'));
 }
 
 function copyCss() {
-  return src('app/renderer/**/*.css').pipe(dest('build/renderer'));
+  return src('app/renderer/**/*.css')
+  .pipe(postcss([require("postcss")(tailwindcss()), autoprefixer()]))
+  .pipe(dest('build/renderer'));
 }
 
 function copyImages() {
